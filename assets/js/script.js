ff18f91e529f93c0aa60728b184e7ff3e1771d07
Perkles <albery.reis.88@gmail.com>
2023-04-22 23:57:07 -0300

    update menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 (function onclickMenu() {
     const menu = document.querySelector('#menu')
     const navbar = document.querySelector('#navbar')
+    const slogan = document.querySelector('#slogan')
     menu.addEventListener('click', function() {
         if (navbar.style.display == 'none') {
             navbar.style.display = 'flex' 
+            slogan.style.margin = '5px'
         } else {
             navbar.style.display = 'none' 
+            slogan.style.marginTop = '260px'
         }
     })
 })()
